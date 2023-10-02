358a7bd71b7cad49665dc60d0deb6d2e64008cda
AndreaFerrara13 <138196137+AndreaFerraray@users.noreply.github.com>
2023-10-02 16:15:08 +0200

    terzo esercizio

---
+++ ThirdEsercixio.java
@@ -0,0 +1,16 @@
+public class ThirdEsercixio {
+    public static void main(String[] args) {
+        double b= 0.5;
+        double h=1.4;
+        double perimetro= (b+h)*2 ;
+        System.out.println("il perimetro è" + perimetro);
+
+
+        short parieDispari= 
+
+    }
+
+
+
+
+}
