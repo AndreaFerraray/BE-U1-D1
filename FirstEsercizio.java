99867b5addcc88c37eb75691e5e68db93c1dd9a1
AndreaFerrara13 <138196137+AndreaFerraray@users.noreply.github.com>
2023-10-02 15:21:04 +0200

    esercizio 1

---
+++ FirstEsercizio.java
@@ -0,0 +1,11 @@
+
+public class Concatena{
+    public static void main(String[] args) {
+        int num3=199;
+                String primo= "ciao";
+        System.out.println( primo + num3);
+
+    };}
+
+
+
